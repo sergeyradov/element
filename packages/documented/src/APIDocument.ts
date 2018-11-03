a36fd76d90baa26b7d2de7df82f90a0c0fa272cf
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2018-11-03 11:29:24 +0100

    Implements proper formatting of call signatures using AST

--- packages/documented/src/APIDocument.ts
+++ packages/documented/src/APIDocument.ts
@@ -2,7 +2,10 @@ import { Document } from './markdown/Document'
 import { ParamType } from './types'
 import { typeToString } from './utils/typeHandling'
 import { TypeObject } from 'typedoc/dist/lib/serialization'
+import { ContentBuilderFn, PhrasingContentBuilder } from './markdown/PhrasingContentBuilder'
+import { CallSignatureWriteParam } from './Parser'
 
+const ascending = (a, b) => (a.toString() || '').localeCompare(b.toString() || '')
 export class APIDocument extends Document {
 	private refMap: Map<string, string>
 	constructor() {
@@ -23,7 +26,7 @@ export class APIDocument extends Document {
 	parameters(
 		params: {
 			name: string
-			type: string
+			type: TypeObject
 			desc?: string
 			isOptional?: boolean
 			defaultValue?: any
@@ -39,28 +42,37 @@ export class APIDocument extends Document {
 							list.item(b => {
 								b.p(c => {
 									c.p(`${name} `)
-									if (type) {
-										c.linkRef(type.trim())
-										c.p(' ')
-									}
-									if (desc) c.p(desc.trim())
+									this.formatType(type, c)
+
+									// if (type) {
+									// 	c.linkRef(type.trim())
+									// }
+									if (desc) c.raw(desc)
+									// b.p(` ${desc.trim()}`)
 								})
 							})
 						} else {
 							list.item(b => {
 								b.p(c => {
-									c.p(`${name.trim()} `)
-									c.linkRef(type.trim())
+									if (isOptional) {
+										c.p(`${name.trim()}? `)
+									} else {
+										c.p(`${name.trim()} `)
+									}
+
+									this.formatType(type, c)
+									// c.linkRef(type.trim())
 									c.p(' ')
 									if (defaultValue !== undefined) {
 										c.p('(Optional, default: ')
 										c.inlineCode(defaultValue.toString().trim())
 										c.p(')')
 									} else if (isOptional) {
-										b.p('(Optional)')
+										c.p('(Optional)')
 									}
 
-									if (desc) b.p(desc.trim())
+									// if (desc) b.p(` ${desc.trim()}`)
+									if (desc) c.raw(desc)
 								})
 							})
 						}
@@ -70,26 +82,112 @@ export class APIDocument extends Document {
 		})
 	}
 
+	formatType(type: TypeObject, c: PhrasingContentBuilder) {
+		switch (type.type) {
+			case 'intrinsic':
+				c.inlineCode(type.name || 'undefined')
+				break
+			case 'stringLiteral':
+				c.p(`"${type.value}"`)
+				break
+			case 'array':
+				if (type.elementType) {
+					// c.text('...')
+					this.formatType(type.elementType, c)
+					c.p('[]')
+				} else {
+					c.inlineCode('any[]')
+				}
+				break
+			case 'union':
+				if (type.types) {
+					type.types.sort(ascending).forEach((type, index, types) => {
+						if (type.name === 'undefined' && type.type === 'intrinsic') {
+						} else {
+							this.formatType(type, c)
+							if (index < types.length - 1) c.p(' | ')
+						}
+					})
+				} else {
+					c.p('undefined')
+				}
+				break
+			case 'reflection':
+				if (type.declaration) console.dir(type)
+				// return reflectedDeclarationToAny(type.declaration as ReflectedDeclaration).toString()
+				break
+			case 'reference':
+				if (type.name === 'Promise') {
+					c.linkRef('Promise', c => {
+						c.p('Promise')
+						c.p('<')
+						if (type.typeArguments)
+							type.typeArguments.forEach((arg, index, args) => {
+								this.formatType(arg, c)
+								if (index < args.length - 1) c.p(' | ')
+							})
+						c.p('>')
+					})
+
+					// return `[[Promise]<${formattedArgs.join(' | ')}>][Promise]`
+				} else {
+					if (type.name) {
+						c.linkRef(type.name, c => {
+							c.inlineCode(type.name || 'undefined')
+						})
+					}
+				}
+				break
+			default:
+				console.log(`Unknown type: ${type.name}`)
+				c.inlineCode('void')
+		}
+	}
+
 	comment(...parts: string[]) {
 		this.block(b => {
 			parts.forEach(text => this.raw(text))
 		})
 	}
 
-	callSignature(
-		params: {
-			name: string
-			type: string
-			desc: string
-			isReference: boolean
-			isOptional: boolean
-			defaultValue: any
-		}[],
-		returnType?: any,
-	) {
+	callSignature(methodName: string, params: CallSignatureWriteParam[], returnType?: any) {
 		this.block(b => {
 			params.forEach(param => {
-				b.h3(param.name)
+				let required = params
+					.filter(p => !p.isOptional)
+					.map(p => {
+						if (
+							p.type.type === 'array' &&
+							p.type.elementType &&
+							p.type.elementType.type === 'intrinsic'
+						) {
+							return `...${p.name}`
+						} else {
+							return p.name
+						}
+					})
+					.join(`, `)
+				let optional = params
+					.filter(p => p.isOptional)
+					.map(p => p.name)
+					.join(`, `)
+
+				let callSignatureString = `${methodName}(${required.length ? required : ''}${
+					optional.length ? `[, ${optional}]` : ''
+				})`
+
+				b.p(c => {
+					b.h3(c => {
+						c.inlineCode(callSignatureString)
+						// c.text(methodName)
+						// c.text('(')
+						// if (required.length) c.text(required)
+						// if (optional.length) c.text(`[, ${optional}]`)
+						// c.text(')')
+					})
+				})
+
+				b.h4('Arguments')
 
 				this.parameters([...params, { name: 'returns:', type: returnType }])
 
