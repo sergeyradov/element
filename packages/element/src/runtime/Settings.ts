f5eef30a2cfba641a513893b73c56d3846148112
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2018-11-03 11:33:08 +0100

    Fix references

--- packages/element/src/runtime/Settings.ts
+++ packages/element/src/runtime/Settings.ts
@@ -201,7 +201,7 @@ export const DEFAULT_SETTINGS: ConcreteTestSettings = {
 /**
  * ConcreteTestSettings represents the minimal set of mandatory settings for a Test to run.
  *
- * Users provide settings in their script via <TestSettings>, which is ultimately merged with DEFAULT_SETTINGS to yield ConcreteTestSettings.
+ * Users provide settings in their script via [TestSettings][], which is ultimately merged with DEFAULT_SETTINGS to yield ConcreteTestSettings.
  *
  * From the Test's perspective, this means that `undefined` checking is front-loaded and  we can simply use settings as-is without having to check values for definedness.
  *
