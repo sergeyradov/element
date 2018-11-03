95ea92e2548ede7f481d30a3db7897519d5e72ce
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2018-11-03 11:29:02 +0100

    Cleanup

--- packages/documented/index.ts
+++ packages/documented/index.ts
@@ -29,42 +29,6 @@ async function main(workspace: string) {
 		console.log(info(`Writing ${filename}`))
 		fs.writeFile(filename, Buffer.from(doc.toMarkdown()))
 	})
-
-	// let compiler = new Compiler(parser)
-	// compiler.compile(destination)
-
-	// // 	var tree = unified()
-	// // 		.use(parse)
-	// // 		.use(frontmatter).parse(`---
-	// // title: Test doc
-	// // ---
-
-	// // # test
-	// // `)
-
-	// let doc = new APIDocument()
-	// // doc.use(abbr)
-
-	// doc.frontmatter({ title: 'Test' })
-	// doc.section('Browser')
-	// doc.comment('This is the first line', 'THis is the second line')
-	// doc.parameter(
-	// 	'name',
-	// 	{ type: 'array', elementType: { type: 'stringLiteral', value: 'test' } },
-	// 	'This is a description',
-	// 	true,
-	// 	true,
-	// )
-
-	// doc.definition(b => {
-	// 	b.footnote('note1', 'First note', c => {
-	// 		c.h1('Test')
-	// 		c.p('text value')
-	// 	})
-	// })
-
-	// // debugger
-	// console.log(doc.toMarkdown())
 }
 
 main(process.argv[2])
